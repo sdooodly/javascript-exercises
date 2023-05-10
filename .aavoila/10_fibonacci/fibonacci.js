371286ab5dc87d2090f2129d90c54732e4984cea
sdooodly <gayathrigs96@gmail.com>
2023-05-10 21:26:44 +0530

    4

--- .aavoila/10_fibonacci/fibonacci.js
+++ .aavoila/10_fibonacci/fibonacci.js
@@ -7,9 +7,6 @@ const fibonacci = n => n < 1 ? "OOPS" : n == 1 || n == 2 ? 1 : (() => {
     }
     return c;
   })();
-      //A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. 
-    //The simplest is the series 1, 1, 2, 3, 5, 8, etc.
-
 
 // Do not edit below this line
 module.exports = fibonacci;
