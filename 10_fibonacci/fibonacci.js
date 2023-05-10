b66091222d77c1a3629ea5a6d5d34a630451ae9e
sdooodly <gayathrigs96@gmail.com>
2023-05-10 21:22:21 +0530

    3

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,14 +1,15 @@
-const fibonacci = (num) => {
-    let sum = 1;
-    for(let a = 1, b = 1, c = 0; c <= num; a = b, b = a + b){
-
-
+const fibonacci = n => n < 1 ? "OOPS" : n == 1 || n == 2 ? 1 : (() => {
+    let a = 1, b = 1, c;
+    for (let i = 3; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
     }
-    return b;
-
-    //A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. 
+    return c;
+  })();
+      //A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. 
     //The simplest is the series 1, 1, 2, 3, 5, 8, etc.
-}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
