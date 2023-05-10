05c6e8a67d7bdddf3b1cd9472a278bb3b958950e
sdooodly <gayathrigs96@gmail.com>
2023-05-10 21:12:06 +0530

    .skip to null

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,8 +1,10 @@
 const fibonacci = (num) => {
     let sum = 1;
-    for(let a = 1; sum <= num; a++){
+    for(let a = 1, b = 1, c = 0; c <= num; a = b, b = a + b){
+
 
     }
+    return b;
 
     //A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. 
     //The simplest is the series 1, 1, 2, 3, 5, 8, etc.
